6402e70d675f9dcd96170872d19cdaec95b19d87
Bård Lahn <bard@lahn.no>
2026-05-22 10:11:17 +0200

    test

--- _includes/css-styles.php
+++ _includes/css-styles.php
@@ -1,3 +1,3 @@
     <link rel="stylesheet" href="<?= $assets_rel_path ?>main.css">
     <meta name="theme-color" content="#f5f5f5" media="(prefers-color-scheme: light)">
-    <meta name="theme-color" content="#a30606" media="(prefers-color-scheme: dark)"> <!-- 1a1a1a -->
+    <meta name="theme-color" content="#1e06a3" media="(prefers-color-scheme: dark)"> <!-- 1a1a1a -->
