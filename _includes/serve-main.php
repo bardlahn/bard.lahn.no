2acadc9d861442ca97543b2ab11c5e76deea7304
Bård Lahn <bard@lahn.no>
2026-08-05 19:51:33 +0200

    Adding top/bottom file-include capability

--- _includes/serve-main.php
+++ _includes/serve-main.php
@@ -61,10 +61,10 @@ include($includes_path."css-styles.php");
 include($includes_path."html-meta.php");
 include($includes_path."scripts-head.php");
 
-if (!empty($fmatter['head-include'])) {
-    // If 'head-include' is set in frontmatter, include the specified file(s)
-    if (is_array($fmatter['head-include'])) {
-        foreach ($fmatter['head-include'] as $file) {
+if (!empty($fmatter['include-head'])) {
+    // If 'include-head' is set in frontmatter, include the specified file(s)
+    if (is_array($fmatter['include-head'])) {
+        foreach ($fmatter['include-head'] as $file) {
             $headInclude = findIncludeFile($file);
             if ($headInclude) {
                 include($headInclude);
@@ -73,11 +73,11 @@ if (!empty($fmatter['head-include'])) {
             }
         }
     } else {
-        $headInclude = findIncludeFile($fmatter['head-include']);
+        $headInclude = findIncludeFile($fmatter['include-head']);
         if ($headInclude) {
             include($headInclude);
         } else {
-            echo "\n<!-- DEBUG: Specified inclusion file not found - " . htmlspecialchars($fmatter['head-include']) . " -->\n";
+            echo "\n<!-- DEBUG: Specified inclusion file not found - " . htmlspecialchars($fmatter['include-head']) . " -->\n";
         }
     }
 }
@@ -91,10 +91,56 @@ if (!empty($fmatter['head-include'])) {
 
 // Printing body elements
 
+if (!empty($fmatter['include-top'])) {
+    // If 'include-top' is set in frontmatter, include the specified file(s)
+    if (is_array($fmatter['include-top'])) {
+        foreach ($fmatter['include-top'] as $file) {
+            $headInclude = findIncludeFile($file);
+            if ($headInclude) {
+                include($headInclude);
+            } else {
+                echo "\n<!-- DEBUG: Specified inclusion file not found - " . htmlspecialchars($file) . " -->\n";
+            }
+        }
+    } else {
+        $headInclude = findIncludeFile($fmatter['include-top']);
+        if ($headInclude) {
+            include($headInclude);
+        } else {
+            echo "\n<!-- DEBUG: Specified inclusion file not found - " . htmlspecialchars($fmatter['include-top']) . " -->\n";
+        }
+    }
+}
+
 include($includes_path."html-nav.php");
 include($includes_path."html-body.php");
 include($includes_path."html-footer.php");
+
+if (!empty($fmatter['include-bottom'])) {
+    // If 'include-bottom' is set in frontmatter, include the specified file(s)
+    if (is_array($fmatter['include-bottom'])) {
+        foreach ($fmatter['include-bottom'] as $file) {
+            $headInclude = findIncludeFile($file);
+            if ($headInclude) {
+                include($headInclude);
+            } else {
+                echo "\n<!-- DEBUG: Specified inclusion file not found - " . htmlspecialchars($file) . " -->\n";
+            }
+        }
+    } else {
+        $headInclude = findIncludeFile($fmatter['include-bottom']);
+        if ($headInclude) {
+            include($headInclude);
+        } else {
+            echo "\n<!-- DEBUG: Specified inclusion file not found - " . htmlspecialchars($fmatter['include-bottom']) . " -->\n";
+        }
+    }
+}
+
 include($includes_path."scripts-body.php");
+
+
+// ...all done!
 
 ?>
 
