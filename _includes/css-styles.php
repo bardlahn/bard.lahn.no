e119bacfd9a1a00d7d0848cf69b29d6a11f402ad
Bård Lahn <bard@lahn.no>
2026-09-19 17:05:37 +0200

    Integrating static file serve in main loop

--- _includes/css-styles.php
+++ _includes/css-styles.php
@@ -1,3 +1,3 @@
-    <link rel="stylesheet" href="https://bl-dev.boksio.net<?= $assets_rel_path ?>main.css">
+    <link rel="stylesheet" href="<?= $assets_rel_path ?>main.css">
     <meta name="theme-color" content="#f5f5f5" media="(prefers-color-scheme: light)">
     <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)">
