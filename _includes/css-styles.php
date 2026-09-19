4cec286433eb341543bb0c1087ea92f973d53a27
Bård Lahn <bard@lahn.no>
2026-09-19 17:02:07 +0200

    Integrating static file serve in main loop

--- _includes/css-styles.php
+++ _includes/css-styles.php
@@ -1,3 +1,3 @@
-    <link rel="stylesheet" href="<?= $assets_rel_path ?>main.css">
+    <link rel="stylesheet" href="<?= $site_config['site-url'] ?><?= $assets_rel_path ?>main.css">
     <meta name="theme-color" content="#f5f5f5" media="(prefers-color-scheme: light)">
     <meta name="theme-color" content="#1a1a1a" media="(prefers-color-scheme: dark)">
